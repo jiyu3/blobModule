5b1a084045bd1544d7480e025cc8377a4d4abf22
Jiyu <modesty.is.virtue@gmail.com>
2016-12-09 13:36:32 +0000

    add create element functions

--- BlobModule.class.js
+++ BlobModule.class.js
@@ -5,35 +5,76 @@
   * @author jiyu ( https://github.com/jiyu3 )
   */
 class BlobModule {
-    /**
-     * Create Blob Object from string.
-     * @param  string content / download file's content
-     * @param  object mine / mine type like {"type": "text/plain"}, see: https://t.co/Z7y0pO62nQ
-     * @return string / blob url like: blob:http://example.com/97ee1f1f-b021-48fa-93c5-4dae85
-     */
-    createBlobURL(content, mine = {"type": "text/plain"}) {
-        return window.URL.createObjectURL(new Blob([content], mine));
+	/**
+	 * Create Blob Object from string.
+	 * @param  object content / download file's row data
+	 * @return string / blob url like: blob:http://example.com/97ee1f1f-b021-48fa-93c5-4dae85
+	 */
+	createBlobURL(content) {
+		return window.URL.createObjectURL(new Blob([content]));
+	}
+
+	/**
+	 * Create DOM element.
+	 * @param  object content / download file's row data
+	 * @return string / blob url like: blob:http://example.com/97ee1f1f-b021-48fa-93c5-4dae85
+	 */
+    createElement(tag_name, ...args) {
+        if(tag_name === null || args.length % 2 !== 0)
+            return false;
+
+        let element = document.createElement(tag_name);
+        for(let i=1; i<arguments.length; i+2)
+            element.setAttribute(args[i], args[i+1]);
+
+        return element;        
     }
 
-    /**
-     * Download data located in blob url.
-     * @param string blob_url / blob url
-     * @param string filename / download filename
-     */
-    downloadBlobURL(blob_url, filename = "download") {
-        let a = document.createElement("a");
-        a.href = blob_url;
-        a.download = filename;
-        a.click();
+	/**
+	 * Create <a> element.
+	 * @param  object content / download file's row data
+	 * @return string / blob url like: blob:http://example.com/97ee1f1f-b021-48fa-93c5-4dae85
+	 */
+    createElementImg(src, ...args) {
+        return this.createElement.bind("img", "src", src)(args);
+    }
+    
+	/**
+	 * Create <audio> element.
+	 * @param  object content / download file's row data
+	 * @return string / blob url like: blob:http://example.com/97ee1f1f-b021-48fa-93c5-4dae85
+	 */
+    createElementAudio(src, ...args) {
+        return this.createElement.bind("audio", "src", src)(args);
     }
 
-    /**
-     * Download text content as file.
-     * @param string content / download file's content
-     * @param object mine / mine type like {"type": "text/plain"}, see: https://t.co/Z7y0pO62nQ
-     * @param string filename / download filename
-     */
-    download(content, mine = {"type": "text/plain"}, filename = "download") {
-        this.downloadBlobURL(this.createBlobURL(content, mine), filename);
+	/**
+	 * Create <video> element.
+	 * @param  object content / download file's row data
+	 * @return string / blob url like: blob:http://example.com/97ee1f1f-b021-48fa-93c5-4dae85
+	 */
+    createElementVideo(src, ...args) {
+        return this.createElement.bind("video", "src", src)(args);        
     }
+
+	/**
+	 * Download data located on blob url.
+	 * @param string blob_url / blob url
+	 * @param string filename / download filename
+	 */
+	downloadFromBlobURL(blob_url, filename = "download") {
+		let a = document.createElement("a");
+		a.href = blob_url;
+		a.download = filename;
+		a.click();
+	}
+
+	/**
+	 * Download content as file.
+	 * @param  object content / download file's row data
+	 * @param string filename / download filename
+	 */
+	download(content, filename = "download") {
+		this.downloadFromBlobURL(this.createBlobURL(content), filename);
+	}
 }
